bae62a1c8d7c03daf8fda510e2b0b27cf91caac2
PeZzZzZz <figuiere.clement@protonmail.com>
2018-04-21 12:18:09 +0000

    git-manage.sh

---
+++ git-manage.sh
@@ -0,0 +1,13 @@
+
+api="$1"
+username="$2"
+password="$3"
+repo_name="$4"
+
+if [[ "$1" == "c" ]]; then
+  curl -u "$username:$password" https://api.github.com/user/repos -d '{"name":"'$repo_name'"}'
+elif [[ "$1" == "d" ]]; then
+  curl -u "$username:$password" -X DELETE https://api.github.com/repos/$username/$repo_name
+else 
+  echo "Param√tre incorrect"
+fi
